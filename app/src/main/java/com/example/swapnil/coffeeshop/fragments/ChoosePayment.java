aff8b12ba44a98e2a012e2e59fd5239917312fbf
Ganpat <ganpatmalekar@gmail.com>
2019-04-26 23:09:00 +0530

    commit

--- app/src/main/java/com/example/swapnil/coffeeshop/fragments/ChoosePayment.java
+++ app/src/main/java/com/example/swapnil/coffeeshop/fragments/ChoosePayment.java
@@ -1,8 +1,12 @@
 package com.example.swapnil.coffeeshop.fragments;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +23,8 @@ public class ChoosePayment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_choose_payment, container, false);
+        View view = inflater.inflate(R.layout.fragment_choose_payment, container, false);
+
+        return view;
     }
 }
